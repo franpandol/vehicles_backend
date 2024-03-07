631386c0c597d32801847f88c258e513ad28203f
FranPandol <pandol.francisco@gmail.com>
2024-03-07 10:16:34 -0300

    Improve admin interface for vehicles and add actions to publish and unpublish

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 
 from vehicles.models import Vehicle, VehicleFeature
 
-admin.site.register(Vehicle)
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ("model", "year", "price", "vehicle_type", "published")
+    search_fields = (
+        "short_name",
+        "full_name",
+        "model",
+    )
+    list_filter = ("vehicle_type", "model", "year")
+    ordering = ("-id",)
+    actions = ["publish_vehicle"]
+
+    def publish_vehicle(self, request, queryset):
+        queryset.update(published=True)
+        self.message_user(request, "Selected vehicles have been published.")
+
+    publish_vehicle.short_description = "Publish selected vehicles"
+
+    def unpublish_vehicle(self, request, queryset):
+        queryset.update(published=False)
+        self.message_user(request, "Selected vehicles have been unpublished.")
+
+    unpublish_vehicle.short_description = "Unpublish selected vehicles"
+
+
+admin.site.register(Vehicle, VehicleAdmin)
+
 admin.site.register(VehicleFeature)
